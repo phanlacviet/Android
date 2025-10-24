a13b5a7b18495f71c58bc7ef649fbeb83380ca35
phanlacviet <phanlacviet578@gmail.com>
2025-10-24 11:13:41 +0700

    Day 07

---
+++ Thực tập/DAY7/ex1.js
@@ -0,0 +1,10 @@
+var name = "Phan Lạc Việt";
+console.log("Welcome to,",name);
+
+age = 22;
+console.log("Welcome to ",name," tuổi: ",age);
+
+let age2 = 20;
+var name2 = "Việt";
+const PI = 3.14;
+
